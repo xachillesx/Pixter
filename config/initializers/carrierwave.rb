d548c26cecb64991693bb0edf3da17defd8fc52e
Ilya Sherstyuk <ilya.sherstyuk@gmail.com>
2014-04-20 02:41:38 +0000

    amazon2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :aws_secret_access_key  => ENV['AWS_ACCESS_KEY'],
-    :aws_access_key_id      => ENV['AWS_SECRET_KEY'],       
+    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],       
     :provider               => 'AWS'                        
   }
   config.fog_directory  = ENV['AWS_BUCKET']                   
